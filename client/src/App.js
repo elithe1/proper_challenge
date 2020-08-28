5ab2b76929b1eb248bfd1b7bc2379c911c5fdeed
eli4trustpilot <elma@trustpilot.com>
2020-08-28 21:56:44 +0200

    init infra added client and api infrastructure added docker compose and docker files

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+    return (<Typography align={"center"}>Nothing to see here</Typography>)
+}
+
+export default App;
